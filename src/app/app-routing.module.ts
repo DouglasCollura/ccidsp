f11cf27fe822406b9618004b80f011fb9efad69a
DouglasCollura <douglascollura@gmail.com>
2023-03-30 09:42:31 -0400

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { NavComponent } from './pages/nav/nav.component';
 
 const routes: Routes = [
   {
     path: '',
     component:LoginComponent,
+  },
+
+  {
+    path: 'user',
+    component:NavComponent,
+    children: [
+      // { path: '', component: BuyerComponent },
+      // { path: 'details', component: BuyerDetailsComponent },
+      // { path: 'search', component: BuyerSearchComponent },
+      // { path: 'rental', component: BuyerRentalComponent },
+      // { path: 'garage', component: BuyerGarageComponent },
+      // { path: 'replacement', component: BuyerReplacementComponent },
+    ]
   },
 ];
 
--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  exports: [
+    MatExpansionModule,
+    MatIconModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
